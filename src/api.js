2ba9e411fdec39465bd80fc363e2f2ae1bcbc364
thiagosouf <thiagosouf@gmail.com>
2022-08-16 12:47:24 -0300

    atualizando api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/",
+  // baseURL: "http://localhost:5000/",
+  baseURL: "http://rifa-eev.herokuapp.com",
 });
 
 export default api;
